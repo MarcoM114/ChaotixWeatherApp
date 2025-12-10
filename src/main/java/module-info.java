8d1bf13db2c9dec1129af2884629b2e87194bced
MarcoMiklautz <marco.miklautz@stud.hcw.ac.at>
2025-12-10 15:01:14 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.chaotixweatherapp {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.chaotixweatherapp to javafx.fxml;
+    exports com.example.chaotixweatherapp;
+}
