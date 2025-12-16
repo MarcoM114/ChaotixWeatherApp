4a03c2ac5e89b21270a134759ccf438812491893
MarcoMiklautz <marco.miklautz@stud.hcw.ac.at>
2025-12-16 18:41:56 +0100

    Grundgerüst für API Abfrage; Code compilierbar

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.example.chaotixweatherapp {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.net.http;
+    requires com.google.gson;
 
 
     opens com.example.chaotixweatherapp to javafx.fxml;
